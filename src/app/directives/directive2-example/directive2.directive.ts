a7badab8f21e49c32fc1284831539e6dbe680c0f
Fernando Arellano <fernando_arellano@epam.com>
2018-09-18 22:47:42 -0500

    Practice 14. @HostListener

--- src/app/directives/directive2-example/directive2.directive.ts
+++ src/app/directives/directive2-example/directive2.directive.ts
@@ -1,12 +1,27 @@
-import {Directive, ElementRef, Renderer} from '@angular/core';
+import {Directive, ElementRef, HostListener, Renderer} from '@angular/core';
 
 @Directive({
   selector: '[appDirective2]'
 })
 export class Directive2Directive {
 
-  constructor(private el: ElementRef, renderer: Renderer) {
-    renderer.setElementStyle(el.nativeElement, 'color', 'yellow');
+  constructor(private el: ElementRef, private renderer: Renderer) {
+   // renderer.setElementStyle(el.nativeElement, 'color', 'yellow');
   }
 
+  @HostListener('mouseover') onMouseOver(){
+    this.ChangeBgColor('yellow');
+  }
+
+  @HostListener('mouseleave') onMouseLeave(){
+    this.ChangeBgColor('black')
+  }
+
+  @HostListener('click') onClick(){
+    window.alert('Host Element Clicked');
+  }
+
+  ChangeBgColor(color: string) {
+    this.renderer.setElementStyle(this.el.nativeElement, 'color', color);
+  }
 }
